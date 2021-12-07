cd22e3848a63aede675268e2b420beb8eb76d510
Rob Grant <rob.grant@sensynehealth.com>
2021-12-07 08:44:30 +0000

    Naming things

--- day7/day7.py
+++ day7/day7.py
@@ -1,11 +1,11 @@
 from raw_input import raw_input
 
 
-def calculate_min_fuel(cost_function):
+def calculate_min_fuel(fuel_rate_modifier):
     return min(
-        sum(cost_function(abs(target - crab)) for crab in raw_input)
+        sum(fuel_rate_modifier(abs(target - crab)) for crab in raw_input)
         for target in range(min(raw_input), max(raw_input) + 1))
 
 
-print(f"Part 1: {calculate_min_fuel(lambda x: x)}")
-print(f"Part 2: {calculate_min_fuel(lambda x: int(x*(x+1)/2))}")
+print(f"Part 1: {calculate_min_fuel(fuel_rate_modifier=lambda x: x)}")
+print(f"Part 2: {calculate_min_fuel(fuel_rate_modifier=lambda x: int(x*(x+1)/2))}")
