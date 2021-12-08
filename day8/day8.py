135b90a85feae2c5c92e0f1d5f278d791a50c6bc
Rob Grant <rob.grant@sensynehealth.com>
2021-12-08 14:47:17 +0000

    Tweak

--- day8/day8.py
+++ day8/day8.py
@@ -3,7 +3,7 @@ from raw_input import raw_input
 lines = raw_input.split("\n")
 
 
-def process_line(line, f):
+def parse_line(line, f):
     ins, outs = map(lambda x: [frozenset(word) for word in line.split(" | ")[x].split(" ")], [0, 1])
     ins.sort(key=lambda x: {int(c): p for p, c in enumerate("234756")}[len(x)])
 
@@ -18,5 +18,5 @@ def process_line(line, f):
     return f(defs, outs)
 
 
-print(f"Part 1: {sum(process_line(l, lambda _, outs: sum(1 for o in outs if len(o) in (2, 4, 3, 7))) for l in lines)}")
-print(f"Part 2: {sum(process_line(l, lambda defs, outs: int(''.join(str(defs[o]) for o in outs)) ) for l in lines)}")
+print(f"Part 1: {sum(parse_line(l, lambda _, outs: sum(str(len(o)) in '2347' for o in outs)) for l in lines)}")
+print(f"Part 2: {sum(parse_line(l, lambda defs, outs: int(''.join(str(defs[o]) for o in outs)) ) for l in lines)}")
