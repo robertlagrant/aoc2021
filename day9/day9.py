b33d26d9825782de228a914a3fc371d6ab04fe51
Rob Grant <rob.grant@sensynehealth.com>
2021-12-09 12:28:51 +0000

    A bit neater

--- day9/day9.py
+++ day9/day9.py
@@ -3,19 +3,13 @@ from itertools import product
 
 from raw_input import raw_input
 
+D = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 R = [[int(c) for c in r] for r in raw_input.split("\n")]
 MAX_Y, MAX_X = len(R), len(R[0])
 
 
-def near(_y, _x, ignore=None, _filter=None):
-    return set(
-        (_y + d_y, _x + d_x)
-        for d_y, d_x in [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        if (not ignore or (_y + d_y, _x + d_x) not in ignore)
-        and 0 <= _y + d_y < MAX_Y
-        and 0 <= _x + d_x < MAX_X
-        and (not _filter or _filter(_y + d_y, _x + d_x))
-    )
+def near(_y, _x):
+    return set((y, x) for dy, dx in D if 0 <= (y := _y + dy) < MAX_Y and 0 <= (x := _x + dx) < MAX_X)
 
 
 def pool(low):
@@ -23,7 +17,7 @@ def pool(low):
 
     while todo and (_next := todo.pop()):
         done.add(_next)
-        todo = todo | near(*_next, ignore=done, _filter=lambda _y, _x: R[_y][_x] != 9)
+        todo = (todo | set((y, x) for y, x in near(*_next) if R[y][x] != 9)) - done
 
     return done
 
