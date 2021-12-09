658efa659f23e6dc8aede3364b34a824f744f080
Rob Grant <rob.grant@sensynehealth.com>
2021-12-09 11:28:07 +0000

    Neater

--- day9/day9.py
+++ day9/day9.py
@@ -1,51 +1,34 @@
-from functools import reduce
+from math import prod
+from itertools import product
 
 from raw_input import raw_input
 
-ROWS = [[int(c) for c in r] for r in raw_input.split("\n")]
-MAX_Y = len(ROWS) - 1
-MAX_X = len(ROWS[0]) - 1
-DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+R = [[int(c) for c in r] for r in raw_input.split("\n")]
+MAX_Y, MAX_X = len(R), len(R[0])
 
 
-def neighbours(_y, _x, ignore=None, f=None):
-    n = set()
-    for d_y, d_x in DIRECTIONS:
-        n_y, n_x = (_y + d_y, _x + d_x)
-
-        if (not ignore or (n_y, n_x) not in ignore) \
-                and 0 <= n_y <= MAX_Y \
-                and 0 <= n_x <= MAX_X \
-                and (not f or f(n_y, n_x)):
-            n.add((n_y, n_x))
-
-    return n
+def near(_y, _x, ignore=None, _filter=None):
+    return set(
+        (_y + d_y, _x + d_x)
+        for d_y, d_x in [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        if (not ignore or (_y + d_y, _x + d_x) not in ignore)
+        and 0 <= _y + d_y < MAX_Y
+        and 0 <= _x + d_x < MAX_X
+        and (not _filter or _filter(_y + d_y, _x + d_x))
+    )
 
 
-lowests = set()
-for y, row in enumerate(ROWS):
-    for x, col in enumerate(row):
-        current = ROWS[y][x]
-        lowest = True
+def pool(low):
+    done, todo = set(), {low}
 
-        for d_y, d_x in neighbours(y, x):
-            if ROWS[d_y][d_x] <= current:
-                lowest = False
+    while todo and (_next := todo.pop()):
+        done.add(_next)
+        todo = todo | near(*_next, ignore=done, _filter=lambda _y, _x: R[_y][_x] != 9)
 
-        if lowest:
-            lowests.add((y, x))
+    return done
 
-pool_sizes = []
-for lowest in lowests:
-    explored = set()
-    to_explore = {lowest}
 
-    while to_explore:
-        candidate = to_explore.pop()
-        explored.add(candidate)
-        to_explore = to_explore | neighbours(*candidate, ignore=explored, f=lambda _y, _x: ROWS[_y][_x] != 9)
+lows = set((y, x) for y, x in product(range(MAX_Y), range(MAX_X)) if all(R[y][x] < R[_y][_x] for _y, _x in near(y, x)))
 
-    pool_sizes.append(len(explored))
-
-print(f"Part 1: {sum(1 + ROWS[y][x] for y, x in lowests)}")
-print(f"Part 2: {reduce((lambda _x, _y: _x * _y), sorted(pool_sizes)[-3:])}")
+print(f"Part 1: {sum(1 + R[y][x] for y, x in lows)}")
+print(f"Part 2: {prod(sorted(map(len, [pool(l) for l in lows]))[-3:])}")
