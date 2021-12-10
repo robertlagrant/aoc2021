b6f448131d47d88bca6cc548a4def991da99f6e5
Rob Grant <rob.grant@sensynehealth.com>
2021-12-10 12:40:19 +0000

    Don't like it still but it'll do for now

--- day10/day10.py
+++ day10/day10.py
@@ -2,17 +2,14 @@ from functools import reduce
 
 from raw_input import raw_input
 
-lines = raw_input.split("\n")
-
 BAROQUE_INFO = [("()", 3, 1), ("[]", 57, 2), ("{}", 1197, 3), ("<>", 25137, 4)]
+PAIRS = {tokens[1]: tokens[0] for tokens, _, _ in BAROQUE_INFO}
+RPAIRS = {b: a for a, b in PAIRS.items()}
+SEVERITY = {tokens[1]: score1 for tokens, score1, _ in BAROQUE_INFO}
+SCORES = {tokens[1]: score2 for tokens, _, score2 in BAROQUE_INFO}
 
-PAIRS = {(b := list(info[0]))[1]: b[0] for info in BAROQUE_INFO}
-RPAIRS = {(b := list(info[0]))[0]: b[1] for info in BAROQUE_INFO}
-SEVERITY = {list(info[0])[1]: info[1] for info in BAROQUE_INFO}
-SCORES = {list(info[0])[1]: info[2] for info in BAROQUE_INFO}
 
-
-def analyse_line(_line, _type=1):
+def analyse_line(_line, _type):
     current_open = ""
     line_corrupt = False
     for c in _line:
@@ -22,14 +19,13 @@ def analyse_line(_line, _type=1):
             if current_open[-1] != PAIRS[c]:
                 line_corrupt = True
                 if _type == 1:
-                    return 1, SEVERITY[c]
+                    return SEVERITY[c]
             current_open = current_open[:-1]
 
     if not line_corrupt and len(current_open) and _type == 2:
         return reduce(lambda x, y: (x * 5) + SCORES[y], "".join(RPAIRS[c] for c in reversed(current_open)), 0)
 
-    return None
 
-
-print(f"Part 1: {sum(s for r, s in [analyse_line(l) for l in lines if analyse_line(l) is not None] if r == 1)}")
+lines = raw_input.split("\n")
+print(f"Part 1: {sum(a for l in lines if (a := analyse_line(l, _type=1)))}")
 print(f"Part 2: {sorted(scores := [a for l in lines if (a := analyse_line(l, _type=2))])[len(scores)//2]}")
