aa03ba4b52933b2348802fe354422f8f1d8eb085
Rob Grant <rob.grant@sensynehealth.com>
2021-12-12 14:09:33 +0000

    Removed some unnecessary logic

--- day12/day12.py
+++ day12/day12.py
@@ -10,13 +10,14 @@ for a, b in [line.split("-") for line in raw_input.split("\n")]:
 
 
 def explore(one_small_dupe=False):
+    c0, c1, c2 = 0, 0, 0
     routes, to_process = set(), ["start"]
     while to_process:
         nodes = (route := to_process.pop()).split(",")
         for next_node in N[nodes[-1]]:
-            if (new_route := f"{route},{next_node}") not in routes and next_node.isupper() or next_node not in nodes \
+            if next_node.isupper() or next_node not in nodes \
                     or (one_small_dupe and all(c == 1 for c in Counter(n for n in nodes if not n.isupper()).values())):
-                routes.add(new_route)
+                routes.add(new_route := f"{route},{next_node}")
                 if next_node != "end":
                     to_process.append(new_route)
     return routes
