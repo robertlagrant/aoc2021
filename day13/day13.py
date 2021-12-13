257590e33faa63369b756b5bcf4d80e83f439104
Rob Grant <rob.grant@sensynehealth.com>
2021-12-13 14:11:03 +0000

    Tidier day 13

--- day13/day13.py
+++ day13/day13.py
@@ -3,43 +3,23 @@ from raw_input import raw
 dots = [tuple(map(int, row.split(","))) for row in raw[0].split("\n")]
 folds = [tuple(row[11:].split("=")) for row in raw[1].split("\n")]
 
-dots_width = max(w for w, h in dots) + 1
-dots_height = max(h for w, h in dots) + 1
+
+def run_dots(_dots, iterations=None):
+    dots_w = max(w for w, _ in dots) + 1
+    dots_h = max(h for _, h in dots) + 1
+    for direction, size in map(lambda f: (f[0], int(f[1])), folds):
+        dots_w = size if direction == "x" else dots_w
+        dots_h = size if direction == "y" else dots_h
+        _dots = set(
+            (x, y - 2 * (y - size)) if direction == "y" and y >= size
+            else (x - 2 * (x - size), y) if direction == "x" and x >= size
+            else (x, y)
+            for x, y in _dots
+        )
+        if iterations is not None and (iterations := iterations - 1) == 0:
+            break
+    return "\n".join("".join("#" if (col, row) in _dots else "." for col in range(dots_w)) for row in range(dots_h))
 
 
-def ppprint(_dots):
-    for row in range(dots_height):
-        print("".join("#" if (col, row) in _dots else "." for col in range(dots_width)))
-
-
-for direction, size in map(lambda f: (f[0], int(f[1])), folds):
-    print(direction, size)
-    new_dots = set()
-    for x, y in dots:
-
-
-        if direction == "y":
-            if y < size:
-                new_dots.add((x, y))
-            else:
-                new_dots.add((x, y - 2 * (y - size)))
-
-            dots_height = size
-        elif direction == "x":
-            if x < size:
-                new_dots.add((x, y))
-            else:
-                new_dots.add((x - 2 * (x - size), y))
-
-        dots_width = size
-
-    dots_width = size if direction == "x" else dots_width
-    dots_height = size if direction == "y" else dots_height
-    dots = new_dots
-    # ppprint(new_dots)
-    # print(len(new_dots))
-
-ppprint(dots)
-
-
-
+print(f"Part 1: {sum([c == '#' for _dots in run_dots(dots, iterations=1) for c in _dots])}")
+print(f"Part 2: \n{run_dots(dots)}")
