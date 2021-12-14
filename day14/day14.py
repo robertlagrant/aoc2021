806528df2bf28429e9a56ebec27fbc25d76f1270
Rob Grant <rob.grant@sensynehealth.com>
2021-12-14 12:47:11 +0000

    Works! But busy.

--- day14/day14.py
+++ day14/day14.py
@@ -1,32 +1,31 @@
 from collections import Counter
 from functools import cache
 
-from test_input import raw
+from raw_input import raw
 
 hull = raw[0]
 rules = {a: a[0] + b + a[1] for a, b in (tuple(rr.split(" -> ")) for rr in raw[1].split("\n"))}
 
+its = 40
+
 
 @cache
-def expand(_in, iterations):
+def expand(_in, iterations) -> Counter:
     if iterations == 0:
-        return _in
+        return Counter(_in[:-1])
 
     if len(_in) == 2:
-        # print("Length is 2!")
         _in = rules[_in] if _in in rules else _in
-        return expand(_in, iterations - 1) if len(_in) > 2 else _in
+        iterations -= 1
 
-    _next = []
+    c = Counter()
     for j in range(len(_in) - 1):
-        _next.append(expand(_in[j:j+2], iterations)[:-1])
+        c2 = expand(_in[j:j+2], iterations)
+        c += c2
 
-    _in = "".join(_next) + _in[-1]
-
-    return _in
+    return c
 
 
-its = 40
-new_hull = expand(hull, its)
+new_hull = expand(hull, its) + Counter([hull[-1]])
 print(new_hull)
 print((c := [b for a, b in Counter(new_hull).most_common()])[0] - c[-1])
