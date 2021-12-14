ddcbedc48fb6474f06bc2fa0c3bcf371d1fba6dc
Rob Grant <rob.grant@sensynehealth.com>
2021-12-14 10:10:29 +0000

    Nice deque. Still far too slow.

--- day14/day14.py
+++ day14/day14.py
@@ -1,20 +1,17 @@
 from collections import Counter, deque
 
-from test_input import raw
+from raw_input import raw
 
 hull = deque(raw[0])
 rules = {a: b for a, b in (tuple(rr.split(" -> ")) for rr in raw[1].split("\n"))}
-print(rules)
 
 for i in range(40):
-    # print("".join(hull))
-    pointer = 0
-    while pointer < len(hull) - 1:
-        if (matched_polymer := hull[pointer] + hull[pointer+1]) in rules:
-            hull.insert(pointer + 1, rules[matched_polymer])
-            pointer += 1
-        pointer += 1
+    print(f"Step {i}")
+    for _ in range(len(hull) - 1):
+        if (matched_polymer := hull[0] + hull[1]) in rules:
+            hull.insert(1, rules[matched_polymer])
+            hull.rotate(-1)
+        hull.rotate(-1)
+    hull.rotate(-1)
 
-# print("".join(hull))
-print(c := [b for a, b in Counter(hull).most_common()])
-print(c[0] - c[-1])
+print((c := [b for a, b in Counter(hull).most_common()])[0] - c[-1])
