fdd820191afc94e24b59ef9f2cca9025b77d5368
Rob Grant <rob.grant@sensynehealth.com>
2021-12-14 09:15:43 +0000

    Very slow deque

--- day14/day14.py
+++ day14/day14.py
@@ -1,23 +1,20 @@
-from collections import Counter
+from collections import Counter, deque
 
 from test_input import raw
 
-template, raw_rules = raw
-rules = {a: b for a, b in (tuple(rr.split(" -> ")) for rr in raw_rules.split("\n"))}
+hull = deque(raw[0])
+rules = {a: b for a, b in (tuple(rr.split(" -> ")) for rr in raw[1].split("\n"))}
 print(rules)
+
 for i in range(40):
-    new_template = ""
-    for j in range(len(template)):
-        if j + 1 < len(template):
-            if (lookup := template[j]+template[j+1]) in rules:
-                new_template += template[j] + rules[lookup]
-            else:
-                new_template += template[j]
-        else:
-            new_template += template[j]
-    template = new_template
+    # print("".join(hull))
+    pointer = 0
+    while pointer < len(hull) - 1:
+        if (matched_polymer := hull[pointer] + hull[pointer+1]) in rules:
+            hull.insert(pointer + 1, rules[matched_polymer])
+            pointer += 1
+        pointer += 1
 
-print(template)
-
-print(c := [b for a, b in Counter(template).most_common()])
+# print("".join(hull))
+print(c := [b for a, b in Counter(hull).most_common()])
 print(c[0] - c[-1])
