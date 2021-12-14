379d3bf9e370c099613cda782c8ef888f43b3f4f
Rob Grant <rob.grant@sensynehealth.com>
2021-12-14 15:37:34 +0000

    Fix for something I don't understand

--- day14/day14.py
+++ day14/day14.py
@@ -8,16 +8,16 @@ RULES = {a: a[0] + b + a[1] for a, b in (tuple(rr.split(" -> ")) for rr in raw[1
 
 
 @cache
-def expand(_in: str, iterations: int, rules) -> Counter:
-    if not iterations or (len(_in) == 2 and _in not in rules):
+def expand(_in: str, iterations: int) -> Counter:
+    if not iterations or (len(_in) == 2 and _in not in RULES):
         return Counter(_in[:-1])
 
-    if _in in rules:
-        _in = rules[_in]
+    if _in in RULES:
+        _in = RULES[_in]
         iterations -= 1
 
     return reduce(lambda c1, c2: c1 + c2, [expand(_in[j:j+2], iterations) for j in range(len(_in) - 1)])
 
 
-print(f"Part 1: {(c := [b for a, b in (expand(HULL, 10, RULES) + Counter([HULL[-1]])).most_common()])[0] - c[-1]}")
-print(f"Part 2: {(c := [b for a, b in (expand(HULL, 40, RULES) + Counter([HULL[-1]])).most_common()])[0] - c[-1]}")
+print(f"Part 1: {(c := [b for a, b in (expand(HULL, 10) + Counter([HULL[-1]])).most_common()])[0] - c[-1]}")
+print(f"Part 2: {(c := [b for a, b in (expand(HULL, 40) + Counter([HULL[-1]])).most_common()])[0] - c[-1]}")
