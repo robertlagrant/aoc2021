1dbac05d4997a9035679dea6421010fe96dd349b
Rob Grant <rob.grant@sensynehealth.com>
2021-12-14 21:44:06 +0000

    Small mods

--- day14/day14.py
+++ day14/day14.py
@@ -4,20 +4,20 @@ from functools import cache, reduce
 from raw_input import raw
 
 HULL = raw[0]
-RULES = {a: a[0] + b + a[1] for a, b in (tuple(rr.split(" -> ")) for rr in raw[1].split("\n"))}
+RULES = {a: a[0] + b + a[1] for a, b in [rr.split(" -> ") for rr in raw[1].split("\n")]}
 
 
 @cache
-def expand(_in: str, iterations: int) -> Counter:
-    if not iterations or (len(_in) == 2 and _in not in RULES):
-        return Counter(_in[:-1])
+def expand(_in: str, its: int) -> Counter:
+    if its == 0 or len(_in) == 2 and _in not in RULES:
+        return Counter(_in[-1])
 
     if _in in RULES:
         _in = RULES[_in]
-        iterations -= 1
+        its -= 1
 
-    return reduce(lambda c1, c2: c1 + c2, [expand(_in[j:j+2], iterations) for j in range(len(_in) - 1)])
+    return reduce(lambda c1, c2: c1 + c2, [expand(_in[j:j+2], its) for j in range(len(_in) - 1)])
 
 
-print(f"Part 1: {(c := [b for a, b in (expand(HULL, 10) + Counter([HULL[-1]])).most_common()])[0] - c[-1]}")
-print(f"Part 2: {(c := [b for a, b in (expand(HULL, 40) + Counter([HULL[-1]])).most_common()])[0] - c[-1]}")
+print(f"Part 1: {(c := [b for a, b in (expand(HULL, 10) + Counter(HULL[-1])).most_common()])[0] - c[-1]}")
+print(f"Part 2: {(c := [b for a, b in (expand(HULL, 40) + Counter(HULL[-1])).most_common()])[0] - c[-1]}")
