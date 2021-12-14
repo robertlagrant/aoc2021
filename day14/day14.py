ecc012607e49b23482440cdfe2da802a27ef07dc
Rob Grant <rob.grant@sensynehealth.com>
2021-12-14 12:17:23 +0000

    Caching solution for day14 still too slow

--- day14/day14.py
+++ day14/day14.py
@@ -1,17 +1,32 @@
-from collections import Counter, deque
+from collections import Counter
+from functools import cache
 
-from raw_input import raw
+from test_input import raw
 
-hull = deque(raw[0])
-rules = {a: b for a, b in (tuple(rr.split(" -> ")) for rr in raw[1].split("\n"))}
+hull = raw[0]
+rules = {a: a[0] + b + a[1] for a, b in (tuple(rr.split(" -> ")) for rr in raw[1].split("\n"))}
 
-for i in range(40):
-    print(f"Step {i}")
-    for _ in range(len(hull) - 1):
-        if (matched_polymer := hull[0] + hull[1]) in rules:
-            hull.insert(1, rules[matched_polymer])
-            hull.rotate(-1)
-        hull.rotate(-1)
-    hull.rotate(-1)
 
-print((c := [b for a, b in Counter(hull).most_common()])[0] - c[-1])
+@cache
+def expand(_in, iterations):
+    if iterations == 0:
+        return _in
+
+    if len(_in) == 2:
+        # print("Length is 2!")
+        _in = rules[_in] if _in in rules else _in
+        return expand(_in, iterations - 1) if len(_in) > 2 else _in
+
+    _next = []
+    for j in range(len(_in) - 1):
+        _next.append(expand(_in[j:j+2], iterations)[:-1])
+
+    _in = "".join(_next) + _in[-1]
+
+    return _in
+
+
+its = 40
+new_hull = expand(hull, its)
+print(new_hull)
+print((c := [b for a, b in Counter(new_hull).most_common()])[0] - c[-1])
