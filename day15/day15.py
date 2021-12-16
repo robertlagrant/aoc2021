562317a43ed26642feda2bc53a46554acdd53c57
Rob Grant <rob.grant@sensynehealth.com>
2021-12-16 00:25:52 +0000

    Different way to do tiling

--- day15/day15.py
+++ day15/day15.py
@@ -4,23 +4,25 @@ import networkx as nx
 
 from test_input import raw
 
+ROWS = [[int(c) for c in r] for r in raw.split("\n")]
+
 
 def shortest_route(tile, map_edge_size=1):
-    g = nx.DiGraph()
-    for row in range(len(tile) * map_edge_size):
-        for col in range(len(tile[0]) * map_edge_size):
-            if col != (len(tile[0]) * map_edge_size) - 1:
-                g.add_edge((col + 1, row), (col, row), weight=tile[row % map_edge_size][col % map_edge_size] + map_edge_size - 1)
-                g.add_edge((col, row), (col + 1, row), weight=tile[row % map_edge_size][(col + 1) % map_edge_size] + map_edge_size - 1)
-            if row != len(tile) * map_edge_size - 1:
-                g.add_edge((col, row), (col, row + 1), weight=tile[(row + 1) % map_edge_size][col % map_edge_size] + map_edge_size - 1)
-                g.add_edge((col, row + 1), (col, row), weight=tile[row % map_edge_size][col % map_edge_size] + map_edge_size - 1)
+    width, height = len(tile[0]), len(tile)
+    G = nx.DiGraph()
+    for i in range(map_edge_size):
+        for row in range(height):
+            for col in range(width):
+                if col != width - 1:
+                    G.add_edge((col + 1, row), (col, row), weight=tile[row][col])
+                    G.add_edge((col, row), (col + 1, row), weight=tile[row][col + 1])
+                if row != height - 1:
+                    G.add_edge((col, row), (col, row + 1), weight=tile[row + 1][col])
+                    G.add_edge((col, row + 1), (col, row), weight=tile[row][col])
 
-    route = nx.shortest_path(g, (0, 0), (len(tile[0] * map_edge_size) - 1, len(tile) * map_edge_size - 1), weight="weight")
+    route = nx.shortest_path(G, (0, 0), (width - 1, height - 1), weight="weight")
 
-    return sum(g.get_edge_data(a, b)["weight"] for a, b in pairwise(route))
+    return sum(G.get_edge_data(a, b)["weight"] for a, b in pairwise(route))
 
 
-ROWS = [[int(c) for c in r] for r in raw.split("\n")]
 print(f"Part 1: {shortest_route(ROWS)}")
-print(f"Part 2: {shortest_route(ROWS, map_edge_size=5)}")
