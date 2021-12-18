f9c0702e6b3c0aa51395c9d388f04f5ad5e3391c
Rob Grant <rob.grant@sensynehealth.com>
2021-12-18 22:58:08 +0000

    Parsing is easy

--- day18/day18.py
+++ day18/day18.py
@@ -1,46 +1,16 @@
-from typing import Tuple
-
 from test_input import raw
 
 
-
-
 class Node:
-    left = None
-    right = None
+    def __init__(self, raw_node):
+        self.left = raw_node[0] if type(raw_node[0]) == int else Node(raw_node[0])
+        self.right = raw_node[1] if type(raw_node[1]) == int else Node(raw_node[1])
 
     def __repr__(self):
-        return f"({str(self.left)}, {str(self.right)})"
-
-
-def parse(s) -> Tuple[Node, str]:
-    if s[0] == "[":
-        # print("1", s)
-        n = Node()
-        if s[1] == "]":
-            # print("2", s)
-            return n, s[2:]
-        elif s[1] == "[":
-            # print("3", s)
-            n.left, s = parse(s[1:])
-            if s.startswith(",["):
-                # print("4", s)
-                n.right, s = parse(s[1:])
-            else:
-                # print("5", s)
-                n.right, s = s[1:].split("]", 1)
-        else:
-            # print("6", s)
-            n.left, s = s[1:].split(",", 1)
-            if s.startswith(",["):
-                # print("7", s)
-                n.right, s = parse(s[1:])
-            else:
-                # print("8", s)
-                n.right, s = s.split("]", 1)
-
-    return n, s
+        return f"Node({self.left}, {self.right})"
 
 
 for line in raw.split("\n"):
-    print(line, eval(line))
+    root = Node(eval(line))
+
+    print(line, root)
