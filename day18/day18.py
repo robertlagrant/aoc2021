41e1b7754cccdbb801fe74127b61746dd65b0066
Rob Grant <rob.grant@sensynehealth.com>
2021-12-19 17:04:32 +0000

    Day 18 little neater

--- day18/day18.py
+++ day18/day18.py
@@ -4,51 +4,34 @@ from itertools import combinations
 
 from raw_input import raw
 
-SIMPLE_MATCH = r"\[(?P<n1>\d+),(?P<n2>\d+)\]"
-
 
 def explode(s):
     i, depth, exploded = 0, 0, False
     while i < len(s) and not exploded:
         inc = 1
-        if m := re.match(SIMPLE_MATCH, s[i:]):
-            if depth >= 4:  # explode
-                exploded = True
+        if m := re.match(r"\[(?P<n1>\d+),(?P<n2>\d+)\]", s[i:]):
+            if exploded := depth >= 4:  # explode
                 s = s[:i] + "0" + s[i + m.end():]  # snip out the exploded bit, replace with "0"
-                prev_len, new_len = 0, 0
-
-                # Search back to find a place for the left num, otherwise discard
-                if n1_m := re.findall(r"(\d+)", s[:i]):
-                    found_num1 = n1_m[-1]
-                    new_num = int(found_num1) + int(m.groupdict()["n1"])
+                if n1_m := re.findall(r"(\d+)", s[:i]):  # Search back to find a place for the left num, else discard
                     prev_len = len(s)
-                    s = s[:s[:i].rfind(found_num1)] + str(new_num) + s[s[:i].rfind(found_num1) + len(found_num1):]
-                    new_len = len(s)
+                    s = s[:s[:i].rfind(n := n1_m[-1])] + str(int(n) + int(m.groupdict()["n1"])) + s[s[:i].rfind(n) + len(n):]
+                    i += len(s) - prev_len
 
-                # Skip the zero
-                i += 1 + new_len - prev_len
-
-                # Search forward to find a place for the right num, otherwise discard
-                if n2_m := re.search(r"(\d+)", s[i:]):
-                    found_num2 = n2_m.groups()[0]
-
-                    new_num = int(found_num2) + int(m.groupdict()["n2"])
-                    s = s[:i] + s[i:].replace(found_num2, str(new_num), 1)
+                i += 1  # Skip the 0
+                if n2_m := re.search(r"(\d+)", s[i:]):  # Search forward to find a place for the right num, else discard
+                    s = s[:i] + s[i:].replace((n := n2_m.groups()[0]), str(int(n) + int(m.groupdict()["n2"])), 1)
             else:
                 inc = m.end()  # skip past the simple match
         elif s[i] == "[":
             depth += 1
         elif s[i] == "]":
             depth -= 1
-
         i += inc
-
     return s
 
 
 def split(s):
     if m := re.search(r"(\d\d+)", s):
-        # print("SPLIT!")
         low = int(m.groups()[0])//2
         high = (int(m.groups()[0])+1)//2
         return f"{s[0:m.start()]}[{low},{high}]{s[m.end():]}"
@@ -56,18 +39,10 @@ def split(s):
 
 
 def reduct(s):
-    cont = True
-    while cont:
-        maybe_exploded = explode(s)
-        if s != maybe_exploded:
-            s = maybe_exploded
-        else:
-            maybe_split = split(s)
-            if s != maybe_split:
-                s = maybe_split
-            else:
-                cont = False
-    return s
+    while True:
+        if s == (maybe_exploded := explode(s)) == (maybe_split := split(s)):
+            return s
+        s = maybe_exploded if maybe_exploded != s else maybe_split
 
 
 def add(s1, s2):
@@ -75,13 +50,8 @@ def add(s1, s2):
 
 
 def magnitude(pair):
-    left, right = pair
-    if not isinstance(left, int):
-        left = magnitude(left)
-    if not isinstance(right, int):
-        right = magnitude(right)
-
-    return 3 * left + 2 * right
+    return 3 * (pair[0] if isinstance(pair[0], int) else magnitude(pair[0])) \
+        + 2 * (pair[1] if isinstance(pair[1], int) else magnitude(pair[1]))
 
 
 print(f"Part 1: " + str(magnitude(eval(reduce(add, raw.split("\n"))))))
