b61c55a5c6580052857ee6c4fce8b3c9ba1a26d7
Rob Grant <rob.grant@sensynehealth.com>
2021-12-20 23:15:43 +0000

    Day 15 under 30 lines!

--- day15/day15.py
+++ day15/day15.py
@@ -12,17 +12,15 @@ def generate_square_map(tile, num_tiles=1):
     ]
 
 
-def shortest_route(tile):
+def shortest_route(t):
     g = nx.DiGraph()
-    for r in range(side := len(tile)):
+    for r in range(side := len(t)):
         for c in range(side):
             if c != side - 1:
-                g.add_edge((c + 1, r), (c, r), weight=tile[r][c])
-                g.add_edge((c, r), (c + 1, r), weight=tile[r][c + 1])
+                g.add_edges_from([((c + 1, r), (c, r), {"w": t[r][c]}), ((c, r), (c + 1, r), {"w": t[r][c + 1]})])
             if r != side - 1:
-                g.add_edge((c, r), (c, r + 1), weight=tile[r + 1][c])
-                g.add_edge((c, r + 1), (c, r), weight=tile[r][c])
-    return nx.shortest_path_length(g, (0, 0), (side - 1, side - 1), weight="weight")
+                g.add_edges_from([((c, r), (c, r + 1), {"w": t[r + 1][c]}), ((c, r + 1), (c, r), {"w": t[r][c]})])
+    return nx.shortest_path_length(g, (0, 0), (side - 1, side - 1), weight="w")
 
 
 ROWS = [[int(c) for c in r] for r in raw.split("\n")]
