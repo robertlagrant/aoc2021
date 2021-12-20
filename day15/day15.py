eeba50b0c738a549e6aaaa7fbfa94c8986867047
Rob Grant <rob.grant@sensynehealth.com>
2021-12-20 23:21:06 +0000

    Day 15 27 lines

--- day15/day15.py
+++ day15/day15.py
@@ -3,8 +3,8 @@ import networkx as nx
 from raw_input import raw
 
 
-def generate_square_map(tile, num_tiles=1):
-    side = len(tile)
+def generate_square_map(_raw, num_tiles=1):
+    side = len(tile := [[int(c) for c in r] for r in _raw.split("\n")])
     return [
         [(n if (n := tile[r_i % side][c_i % side] + (r_i // side) + (c_i // side)) <= 9 else n - 9)
          for c_i in range(side * num_tiles)]
@@ -16,13 +16,12 @@ def shortest_route(t):
     g = nx.DiGraph()
     for r in range(side := len(t)):
         for c in range(side):
-            if c != side - 1:
+            if c < side - 1:
                 g.add_edges_from([((c + 1, r), (c, r), {"w": t[r][c]}), ((c, r), (c + 1, r), {"w": t[r][c + 1]})])
-            if r != side - 1:
+            if r < side - 1:
                 g.add_edges_from([((c, r), (c, r + 1), {"w": t[r + 1][c]}), ((c, r + 1), (c, r), {"w": t[r][c]})])
     return nx.shortest_path_length(g, (0, 0), (side - 1, side - 1), weight="w")
 
 
-ROWS = [[int(c) for c in r] for r in raw.split("\n")]
-print(f"Part 1: {shortest_route(generate_square_map(ROWS))}")
-print(f"Part 2: {shortest_route(generate_square_map(ROWS, num_tiles=5))}")
+print(f"Part 1: {shortest_route(generate_square_map(raw))}")
+print(f"Part 2: {shortest_route(generate_square_map(raw, num_tiles=5))}")
